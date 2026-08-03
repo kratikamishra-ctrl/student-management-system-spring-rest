a6a7ce3f82e86021cc49e0e9d109e1ad5b0564fb
Vikky <vikky_12@gmail.com>
2026-08-03 10:36:07 +0530

    init commit

---
+++ src/main/java/com/example/studentmanagementsystemspringrest/StudentManagementSystemSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystemspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementSystemSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmanagementsystemspringrest/StudentManagementSystemSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystemspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
